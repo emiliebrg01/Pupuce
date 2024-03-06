7e9b13ab63bcbfdc61137678d827cafbe7b9e338
emilie bourg <emilie.bourg@epfedu.fr>
2024-03-06 19:38:13 +0100

    bug

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ var config = {
       debug: false // permet de voir les hitbox et les vecteurs d'acceleration quand mis à true
     }
   },
-  scene: [accueil, selection, niveauboss, niveau1, niveau2, victoire] 
+  scene: [selection, niveauboss, niveau1, niveau2] 
   
 };
 
